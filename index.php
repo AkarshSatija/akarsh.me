e385506c31056e153e1537f5d0089d33a1e89f4c
Akarsh <akarsh.satija@gmail.com>
2013-12-01 18:49:21 +0530

    INIT with old work

--- index.php
+++ index.php
@@ -1,1 +1,103 @@
-<?php echo 'working' ?>
+<!DOCTYPE html>
+
+<html lang="en">
+<meta style="visibility: hidden !important; display: block !important; width: 0px !important; height: 0px !important; border-style: none !important;">
+<embed id="__IDM__" type="application/x-idm-downloader" width="1" height="1" style="visibility: hidden !important; display: block !important; width: 1px !important; height: 1px !important; border-style: none !important; position: absolute !important; top: 0px !important; left: 0px !important;">
+  </meta>
+<head>
+
+<!-- fonts -->
+
+<script src="http://use.edgefonts.net/mountains-of-christmas:n4,n7:all.js"></script>
+<script src="http://use.edgefonts.net/de-walpergens-pica-small-caps.js"></script>
+
+<!-- fonts -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<title>Akarsh Satija</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Akarsh Satija Personal Portfolio">
+<meta name="author" content="Akarsh Satija">
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
+      </script>
+    <![endif]-->
+<link href="css/theme.css" rel="stylesheet">
+<link href="css/theme-responsive.css" rel="stylesheet">
+<link href="css/layout.css" rel="stylesheet">
+<script src="js/jquery.js"></script>
+<script src="js/theme.js"></script>
+<script src="js/main.js"></script>
+<script src="js/texteffect.js"></script>
+<style>
+.social li a img {
+	width:55%;
+	height:auto;
+}
+.row-fluid .offset6:first-child {
+	margin-left: 33.282%;
+}
+.row-fluid .span5 {
+	width: 46.171%;
+}
+.rightbar {
+	width:10%;
+	position:absolute;
+	right:0;
+	bottom:0;
+	top:0;
+	padding-top:7%;
+	/*z-index:-1;*/
+	background:rgba(0, 0, 0, 0.667);
+}
+.rightbar ul {
+	list-style:none;
+	display:block;
+	height:100%;
+	margin:auto;
+	padding:auto;
+}
+.rightbar ul li {
+	display:block;
+	height:25%;
+	margin:auto;
+	padding:auto;
+}
+</style>
+</head>
+<body class="home" data-twttr-rendered="true" ginger_sofatware_stylesheet="true" ginger_sofatware_doc="true">
+
+<!-- =====================[ Navbar ]================== -->
+<div class="navbar navbar-fixed-top  ">
+  <div class="navbar-inner navbar-inverse">
+    <div class="container-fluid"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a> <a class="brand" style="font-family:de-walpergens-pica-small-caps, serif; color:#fff;font-size: 2.0em;" href="/">Akarsh Satija</a>
+      <div class="nav-collapse">
+        <ul class="nav pull-right">
+          <li><a href="/about">About</a></li>
+          <li><a href="/connect">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="home_container" style="height: 592px;"> <img class="landing_img" src="img/Akarsh_landing.png" alt="Model 1">
+  <div class="row-fluid">
+    <div class="span5 offset6">
+      <div class="home_box">
+        <h2 class="title" style="font-family: mountains-of-christmas, serif; font-weight:bold;">A k a r s h&nbsp;&nbsp;&nbsp;S a t i j a</h2>
+        <span class="subtitle"><br>
+        Web Developer | Innovative Thinker | Creative Doer </span> <br>
+      </div>
+    </div>
+    <div class="rightbar">
+      <ul class="social">
+        <li> <a href="https://www.facebook.com/satija.akarsh" target="_blank"><img src="img/1371993952_facebook.png"></a> </li>
+        <li> <a href="https://twitter.com/akarshsatija" target="_blank"><img src="img/1371993960_twitter.png"></a> </li>
+        <li> <a href="mailto:akarsh.satija@gmail.com" target="_blank"><img src="img/1371993879_gmail.png"></a> </li>
+        <li> <a href="http://in.linkedin.com/in/akarshsatija/" target="_blank"><img src="img/1371993969_linkedin.png"></a> </li>
+      </ul>
+    </div>
+  </div>
+</div>
+</body>
+</html>
