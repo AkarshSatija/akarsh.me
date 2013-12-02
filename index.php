79dcdd49ccbc97e01cbb2d918ede213fde0b92b0
Akarsh <akarsh.satija@gmail.com>
2013-12-02 13:30:25 +0530

    Putting Google Analytics

--- index.php
+++ index.php
@@ -11,6 +11,17 @@
 <script src="http://use.edgefonts.net/mountains-of-christmas:n4,n7:all.js"></script>
 <script src="http://use.edgefonts.net/de-walpergens-pica-small-caps.js"></script>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46142311-1', 'akarsh.in');
+  ga('send', 'pageview');
+
+</script>
+
 <!-- fonts -->
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta charset="utf-8">
