620cb8e3e54d47782ba8d4f281ce5790079d011f
Akarsh <akarsh.satija@gmail.com>
2013-12-08 14:26:35 +0530

    Installing Event tracking

--- header.php
+++ header.php
@@ -12,6 +12,19 @@
   ga('send', 'pageview');
 
 </script>
+<script type="text/javascript">
+//GA for event tracking
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-46142311-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 
 <script src="http://use.edgefonts.net/mountains-of-christmas:n4,n7:all.js"></script>
 <script src="http://use.edgefonts.net/de-walpergens-pica-small-caps.js"></script>
@@ -50,7 +63,7 @@
     <div class="container-fluid"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a> <a class="brand" style="font-family:de-walpergens-pica-small-caps, serif; color:#fff;font-size: 2.0em;" href="/">Akarsh Satija</a>
       <div class="nav-collapse">
         <ul class="nav pull-right">
-          <li><a href="/about.php">About</a></li>
+          <li><a href="/about.php" onClick="_gaq.push(['_trackEvent', 'Navigation', 'Go', 'About']);">About</a></li>
           <!--li><a href="/connect">Contact</a></li-->
         </ul>
       </div>
--- index.php
+++ index.php
@@ -44,15 +44,4 @@
         Web Developer | Innovative Thinker | Creative Doer </span> <br>
       </div>
     </div>
-    <div class="rightbar">
-      <ul class="social">
-        <li> <a href="https://www.facebook.com/satija.akarsh" target="_blank"><img src="img/1371993952_facebook.png"></a> </li>
-        <li> <a href="https://twitter.com/akarshsatija" target="_blank"><img src="img/1371993960_twitter.png"></a> </li>
-        <li> <a href="mailto:akarsh.satija@gmail.com" target="_blank"><img src="img/1371993879_gmail.png"></a> </li>
-        <li> <a href="http://in.linkedin.com/in/akarshsatija/" target="_blank"><img src="img/1371993969_linkedin.png"></a> </li>
-      </ul>
-    </div>
-  </div>
-</div>
-</body>
-</html>
+ <?php include("footer.php") ; ?>
