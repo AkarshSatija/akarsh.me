9fe886aa27133b736af9d980d140a70d0c2f46d3
Akarsh Satija <akarsh.satija@gmail.com>
2014-02-12 11:29:09 +0530

    Aff code

---
+++ aff.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	Akarsh.in
+</head>
+<body>
+<?php include("header.php") ; ?>
+
+<iframe src="http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-139218464469253438" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" ></iframe>
+</body>
+</html>
