066f05c82b2516ce215967e3d9fd015e7411cc06
Akarsh <akarsh.satija@gmail.com>
2014-04-03 10:53:22 +0530

    adding searchbar

--- offer.php
+++ offer.php
@@ -4,7 +4,11 @@
 </head>
 <body>
 <?php include("header.php") ; ?>
-
+Moto G
 <iframe src="http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-139218464469253438" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" ></iframe>
+
+Search Bar:
+<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138648042608842506' height=55 width=660 scrolling='no' frameborder=0></iframe>
+
 </body>
 </html>
