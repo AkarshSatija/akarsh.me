a8c2f2cc70a717f929490f6a67264bab6504199a
Akarsh <akarsh.satija@gmail.com>
2015-12-19 23:40:35 +0530

    updated with new content

--- index.php
+++ index.php
@@ -24,26 +24,28 @@
     .theme .introduction .content {
         margin-top: 70%;
     }
-    
     .about p {
         width: 100% !important;
         margin: 0 !important;
     }
-    .home.wrapper
-    {
-        position:absolute !important;
-        z-index:3 !important;
-        margin-left: -170px; 
-        height:100%;
+    .home.wrapper {
+        position: absolute !important;
+        z-index: 3 !important;
+        margin-left: -170px;
+        height: 100%;
         left: 50%;
-        padding-top:10px;
+        padding-top: 10px;
     }
-    @media screen and (max-width: 800px)
-    {
-        .home.wrapper
-        {
-            margin-left: 0px; width:100%;left: 0;padding-top:10px;
-            height:auto;
+    a {
+    	text-decoration: none !important;
+    }
+    @media screen and (max-width: 800px) {
+        .home.wrapper {
+            margin-left: 0px;
+            width: 100%;
+            left: 0;
+            padding-top: 10px;
+            height: auto;
         }
     }
     </style>
@@ -54,19 +56,21 @@
         <div>
             <a href="#about" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'About']);"><small>About</small></a>
             <a href="#contact" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Contact']);"><small>Contact</small></a>
-            <a href="./resume.pdf" target="_blank" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Resume']);"><small>Resume</small></a>
+            <!--  <a href="./resume.pdf" target="_blank" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Resume']);"><small>Resume</small></a> -->
+            <a target="_blank" href="https://akarshsatija.wordpress.com/" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'BLOG']);"><small>Blog</small></a>
         </div>
     </nav>
     <section class="introduction background color dark image quarter-opacity " style="  height: 100%;" id="5538d9444a3a6300034008dd">
         <div id="particles-js" style="position: absolute;background-image:url(//s3.amazonaws.com/branded.me/gallery/architecture/16.jpg)">
         </div>
-        <div class="wrapper home" >
+        <div class="wrapper home">
             <nav style="margin-left: -85%;">
                 <a href="#about" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'About']);"><small>About</small></a>
                 <a href="#contact" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Contact']);"><small>Contact</small></a>
-                <a href="./resume.pdf"  target="_blank" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Resume']);"><small>Resume</small></a>
+                <!--  <a href="./resume.pdf" target="_blank" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Resume']);"><small>Resume</small></a> -->
+                <a target="_blank" href="https://akarshsatija.wordpress.com/" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'BLOG']);"><small>Blog</small></a>
             </nav>
-            <div class="clearfix content image-center image-medium"><img alt="Akarsh Satija" class="circle" src="./assets/0_-76t66yd1VA-DtGJBDEy_bjIqUm-mzCJnRZ0hFf5pffAoqrBqxbYCDHX02P.jpg">
+            <div class="clearfix content image-center image-medium"><img alt="Akarsh Satija" class="circle" src="./akarsh.jpg">
                 <div class="text-wrapper">
                     <div class="text">
                         <h1>Akarsh Satija</h1>
@@ -92,13 +96,12 @@
                     <h2 style="text-align:center;">About Akarsh</h2>
                     <div class="clearfix custom-content inherit">
                         <p>
-                            Akarsh is a developer who loves the web. He’s thinking a lot about how to make web and web tools slicker, sexier and interactive.
+                            Akarsh is a developer who loves solving problems. He’s thinking a lot about how to make web tools and web apps smarter, slicker and sexier.
                             <br>
-                            <br> With a <b>BTech in CompSci</b>, he is already adept with Various technology Stacks in Backend as well as FrontEnd, and is a sure shot to master many more in time! He plied his trade of web development at <b>ITmagia</b> before joining <b>SoLoMoSalsa</b>
+                            <br> With a <b>BTech in CompSci</b>, He is already adept with various technology Stacks in Backend as well as FrontEnd, and is a sure shot to master many more in time! He plied his trade of web development at <b><a href="solomosalsa.com" target="_blank" onClick="_gaq.push(['_trackEvent', 'Link', 'Click', 'solomosalsa']);">SoLoMoSalsa</a></b> before joining <b><a onClick="_gaq.push(['_trackEvent', 'Link', 'Click', 'lbb']);" target="_blank" href="https://lbb.in">Little Black Book</a></b>
                             <br>
-                            <br> When Akarsh is not dreaming in algorithms and scripting, He is found contributing at different Tech events. He is working as a <b>Team Lead in Technology at SoLoMoSalsa</b> and answers to all the complicated engineering problems thrown at him there. He is also adept at working with Facebook APIs, Twitter APIs and many other social platforms.
-                            <br>
-                            <br> Currently proficient in various backend and front-end Web-Dev technologies like <b>Node JS</b>, <b>Javascript</b>, <b>PHP</b>, <b>HTML5</b>, <b>CSS3</b>, <b>RestFul APIs</b>... But we can't guarantee that this list is exhaustive. He learns so fast!</p>
+                            <br> When Akarsh is not dreaming in algorithms and scripting, He is found contributing at different Tech events. 
+                            <br>He is working as a <b>Full Stack Lead at Little Black Book</b> and answers to all the complicated engineering problems thrown at him there. He is also adept at DevOps and building WebApps.
                     </div>
                 </div>
             </div>
@@ -114,14 +117,14 @@
                     </div>
                     <div class="info">
                         <!-- <div class="location">
-                            <h3>New Delhi Area, India</h3>
-                        </div>
-                        <div class="phone">
-                            <h3><a href="tel:9017050076">9017050076</a></h3>
-                        </div>
-                        <div class="email">
-                            <h3><a href="mailto:akarsh.satija@gmail.com">akarsh.satija@gmail.com</a></h3>
-                        </div> -->
+<h3>New Delhi Area, India</h3>
+</div>
+<div class="phone">
+<h3><a href="tel:9017050076">9017050076</a></h3>
+</div>
+<div class="email">
+<h3><a href="mailto:akarsh.satija@gmail.com">akarsh.satija@gmail.com</a></h3>
+</div> -->
                     </div>
                     <div class="social">
                         <a class="facebook" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'facebook']);" href="http://facebook.com/satija.akarsh" target="_blank"></a>
@@ -147,7 +150,6 @@
         a.src = g;
         m.parentNode.insertBefore(a, m)
     })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
     ga('create', 'UA-46142311-1', 'akarsh.in');
     ga('send', 'pageview');
     </script>
@@ -165,23 +167,21 @@
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(ga, s);
     })();
-
-
-//smooth scroll
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+    //smooth scroll
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
     </script>
     <link rel="stylesheet" media="screen" href="./bower_components/particles.js/demo/css/style.css">
     <script src="./bower_components/particles.js/particles.js"></script>
