18e25ef31593cff4ab89e7fa8be02841874e7539
Akarsh <akarsh.satija@gmail.com>
2017-04-15 00:02:49 +0530

    added meta

--- index.php
+++ index.php
@@ -1,193 +1,199 @@
-<?php echo '<!-- new -->'; ?>
+<?php
+/* Init composer */
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="IE=edge" http-equiv="X-UA-Compatible">
-    <meta content="Akarsh Satija is Code Chef. View Akarsh&#39;s Resume, Biography and more. Get in touch with Akarsh today." name="description">
-    <meta content="summary_large_image" name="twitter:card">
-    <meta name="google-site-verification" content="psXXXk52vCqjJ1IA0_0ZmfbPNigNqqNUn0iotbrqxeU" />
-    <meta content="Akarsh Satija | Code Chef | Bio &amp; Portfolio" name="twitter:title">
-    <meta content="Akarsh Satija is | Code Chef. View Akarsh&#39;s Resume, Biography and more. Get in touch with Akarsh today." name="twitter:description">
-    <meta content="./assets/0_-76t66yd1VA-DtGJBDEy_bjIqUm-mzCJnRZ0hFf5pffAoqrBqxbYCDHX02P.jpg" name="twitter:image">
-    <meta content="" name="twitter:url">
-    <title>Akarsh Satija | Code Chef | Portfolio</title>
-    <link href="shared/img/favicon.png" rel="icon" type="image/png">
-    <link href="./assets/css.css" rel="stylesheet" type="text/css">
-    <link href="./assets/reset.css" rel="stylesheet" type="text/css">
-    <link href="./assets/clearfix.css" rel="stylesheet" type="text/css">
-    <link href="./assets/style.css" rel="stylesheet" type="text/css">
-    <link href="./assets/mobile.css" media="(max-width: 800px)" rel="stylesheet" type="text/css">
-    <link href="./assets/default.css" rel="stylesheet" type="text/css">
-    <style>
-    .theme .introduction .content {
-        margin-top: 70%;
-    }
-    .about p {
-        width: 100% !important;
-        margin: 0 !important;
-    }
-    .home.wrapper {
-        position: absolute !important;
-        z-index: 3 !important;
-        margin-left: -170px;
-        height: 100%;
-        left: 50%;
-        padding-top: 10px;
-    }
-    a {
-    	text-decoration: none !important;
-    }
-    @media screen and (max-width: 800px) {
-        .home.wrapper {
-            margin-left: 0px;
-            width: 100%;
-            left: 0;
-            padding-top: 10px;
-            height: auto;
-        }
-    }
-    </style>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta name="description" content="Akarsh Satija is Code Chef. View Akarsh&#39;s Resume, Biography and more. Get in touch with Akarsh today.">
+
+	<meta name="google-site-verification" content="psXXXk52vCqjJ1IA0_0ZmfbPNigNqqNUn0iotbrqxeU" />
+
+	<link rel="canonical" href="https://akarsh.me/">
+	<link rel="preconnect" href="//www.google-analytics.com">
+	<title>Akarsh Satija | Code Chef | Portfolio</title>
+
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@AkarshSatija">
+	<meta name="twitter:domain" content="akarsh.me">
+	<meta name="twitter:creator" content="@AkarshSatija">
+	<meta name="twitter:image" content="https://akarsh.me/akarsh.jpg">
+	<meta name="twitter:title" content="Akarsh Satija | Code Chef | Bio &amp; Portfolio" >
+	<meta name="twitter:description" content="Akarsh Satija | Code Chef. View Akarsh&#39;s Resume, Biography and more. Get in touch with Akarsh." >
+
+	<meta property="og:url" content="https://akarsh.me/" />
+	<meta property="og:type" content="Portfolio" />
+	<meta property="og:title" content="Akarsh Satija" />
+	<meta property="og:description" content="Code Chef | Hacker | Webifier | Geek | All time Developer" />
+	<meta property="og:image" content="http://ravisuhag.com/assets/images/ravisuhag.png" />
+
+
+	<link href="shared/img/favicon.png" rel="icon" type="image/png">
+	<link href="./assets/css.css" rel="stylesheet" type="text/css">
+	<link href="./assets/reset.css" rel="stylesheet" type="text/css">
+	<link href="./assets/clearfix.css" rel="stylesheet" type="text/css">
+	<link href="./assets/style.css" rel="stylesheet" type="text/css">
+	<link href="./assets/mobile.css" media="(max-width: 800px)" rel="stylesheet" type="text/css">
+	<link href="./assets/default.css" rel="stylesheet" type="text/css">
+	<style>
+	.theme .introduction .content {
+		margin-top: 70%;
+	}
+	.about p {
+		width: 100% !important;
+		margin: 0 !important;
+	}
+	.home.wrapper {
+		position: absolute !important;
+		z-index: 3 !important;
+		margin-left: -170px;
+		height: 100%;
+		left: 50%;
+		padding-top: 10px;
+	}
+	a {
+		text-decoration: none !important;
+	}
+	@media screen and (max-width: 800px) {
+		.home.wrapper {
+			margin-left: 0px;
+			width: 100%;
+			left: 0;
+			padding-top: 10px;
+			height: auto;
+		}
+	}
+	</style>
 </head>
 
 <body class="theme">
-    <nav><a class="hamburger"><small>Menu</small></a>
-        <div>
-            <a href="#about" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'About']);"><small>About</small></a>
-            <a href="#contact" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Contact']);"><small>Contact</small></a>
-            <!--  <a href="./resume.pdf" target="_blank" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Resume']);"><small>Resume</small></a> -->
-            <a target="_blank" href="https://akarshsatija.wordpress.com/" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'BLOG']);"><small>Blog</small></a>
-        </div>
-    </nav>
-    <section class="introduction background color dark image quarter-opacity " style="  height: 100%;" id="5538d9444a3a6300034008dd">
-        <div id="particles-js" style="position: absolute;background-image:url(./assets/img/16.jpg)">
-        </div>
-        <div class="wrapper home">
-            <nav style="margin-left: -85%;">
-                <a href="#about" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'About']);"><small>About</small></a>
-                <a href="#contact" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Contact']);"><small>Contact</small></a>
-                <!--  <a href="./resume.pdf" target="_blank" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Resume']);"><small>Resume</small></a> -->
-                <a target="_blank" href="https://akarshsatija.wordpress.com/" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'BLOG']);"><small>Blog</small></a>
-            </nav>
-            <div class="clearfix content image-center image-medium"><img alt="Akarsh Satija" class="circle" src="./akarsh.jpg">
-                <div class="text-wrapper">
-                    <div class="text">
-                        <h1>Akarsh Satija</h1>
-                        <p>Code Chef | Hacker | Webifier | Geek</p>
-                        <div class="social">
-                            <a class="facebook" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'facebook']);" href="http://facebook.com/satija.akarsh" target="_blank"></a>
-                            <a class="twitter" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'twitter']);" href="http://twitter.com/@akarshsatija" target="_blank"></a>
-                            <a class="linkedin" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'linkedin']);" href="https://www.linkedin.com/in/akarshsatija" target="_blank"></a>
-                            <a class="github" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'github']);" href="http://github.com/akarshsatija" target="_blank"></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <a class="continue" href="#about"><small>More</small></a>
-        </div>
-    </section>
-    <section class="about custom dark image quarter-opacity " id="about">
-        <div style="position: absolute;background-image:url(./assets/img/7.jpg)">
-        </div>
-        <div class="wrapper">
-            <div class="clearfix content one-col">
-                <div class="col">
-                    <h2 style="text-align:center;">About Akarsh</h2>
-                    <div class="clearfix custom-content inherit">
-                        <p>
-                            Akarsh is a developer who loves solving problems. He’s thinking a lot about how to make web tools and web apps smarter, slicker and sexier.
-                            <br>
-                            <br> With a <b>BTech in CompSci</b>, He is already adept with various technology Stacks in Backend as well as FrontEnd, and is a sure shot to master many more in time! He plied his trade of web development at <b><a href="//solomosalsa.com" target="_blank" onClick="_gaq.push(['_trackEvent', 'Link', 'Click', 'solomosalsa']);">SoLoMoSalsa</a></b> before joining <b><a onClick="_gaq.push(['_trackEvent', 'Link', 'Click', 'lbb']);" target="_blank" href="https://lbb.in">Little Black Book</a></b>
-                            <br>
-                            <br>When Akarsh is not dreaming in algorithms and scripting, He is found contributing at different Tech events.
-                            <br>He is working as a <b>Engineering Manager at Little Black Book</b> and answers to all the complicated engineering problems thrown at him there. He is also adept at DevOps and architecturing + building WebApps.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="contact background color dark image quarter-opacity " id="contact" style="background-image:url(./assets/img/9.jpg)">
-        <div class="wrapper">
-            <div class="clearfix content">
-                <div class="">
-                    <div class="clearfix description">
-                        <h2>Contact</h2>
-                        <p>Get in touch with me if you would like to do business together, share an idea or just say hello.</p>
-                    </div>
-                    <div class="info">
-                        <?php /* <div class="location">
-<h3>New Delhi Area, India</h3>
-</div>
-<div class="phone">
-<h3><a href="tel:9017050076">9017050076</a></h3>
-</div>
-<div class="email">
-<h3><a href="mailto:akarsh.satija@gmail.com">akarsh.satija@gmail.com</a></h3>
-</div> */ ?>
-                    </div>
-                    <div class="social">
-                        <a class="facebook" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'facebook']);" href="http://facebook.com/satija.akarsh" target="_blank"></a>
-                        <a class="twitter" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'twitter']);" href="http://twitter.com/@akarshsatija" target="_blank"></a>
-                        <a class="linkedin" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'linkedin']);" href="https://www.linkedin.com/in/akarshsatija" target="_blank"></a>
-                        <a class="github" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'github']);" href="http://github.com/akarshsatija" target="_blank"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <script src="./assets/jquery.min.js"></script>
-    <script>
-    //GA code
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-    ga('create', 'UA-46142311-1', 'akarsh.in');
-    ga('send', 'pageview');
-    </script>
-    <script type="text/javascript">
-    //GA for event tracking
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-46142311-1']);
-    _gaq.push(['_trackPageview']);
+	<nav><a class="hamburger"><small>Menu</small></a>
+		<div>
+			<a href="#about" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'About']);"><small>About</small></a>
+			<a href="#contact" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Contact']);"><small>Contact</small></a>
+			<!--  <a href="./resume.pdf" target="_blank" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Resume']);"><small>Resume</small></a> -->
+			<a target="_blank" href="https://akarshsatija.wordpress.com/" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'BLOG']);"><small>Blog</small></a>
+		</div>
+	</nav>
+	<section class="introduction background color dark image quarter-opacity " style="  height: 100%;" id="5538d9444a3a6300034008dd">
+		<div id="particles-js" style="position: absolute;background-image:url(./assets/img/16.jpg)">
+		</div>
+		<div class="wrapper home">
+			<nav style="margin-left: -85%;">
+				<a href="#about" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'About']);"><small>About</small></a>
+				<a href="#contact" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Contact']);"><small>Contact</small></a>
+				<!--  <a href="./resume.pdf" target="_blank" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'Resume']);"><small>Resume</small></a> -->
+				<a target="_blank" href="https://akarshsatija.wordpress.com/" onClick="_gaq.push(['_trackEvent', 'NAV', 'Go', 'BLOG']);"><small>Blog</small></a>
+			</nav>
+			<div class="clearfix content image-center image-medium"><img alt="Akarsh Satija" class="circle" src="./akarsh.jpg">
+				<div class="text-wrapper">
+					<div class="text">
+						<h1>Akarsh Satija</h1>
+						<p>Code Chef | Hacker | Webifier | Geek</p>
+						<div class="social">
+							<a class="facebook" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'facebook']);" href="http://facebook.com/satija.akarsh" target="_blank"></a>
+							<a class="twitter" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'twitter']);" href="http://twitter.com/@akarshsatija" target="_blank"></a>
+							<a class="linkedin" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'linkedin']);" href="https://www.linkedin.com/in/akarshsatija" target="_blank"></a>
+							<a class="github" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'github']);" href="http://github.com/akarshsatija" target="_blank"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<a class="continue" href="#about"><small>More</small></a>
+		</div>
+	</section>
+	<section class="about custom dark image quarter-opacity " id="about">
+		<div style="position: absolute;background-image:url(./assets/img/7.jpg)">
+		</div>
+		<div class="wrapper">
+			<div class="clearfix content one-col">
+				<div class="col">
+					<h2 style="text-align:center;">About Akarsh</h2>
+					<div class="clearfix custom-content inherit">
+						<p>
+							Akarsh is a developer who loves solving problems. He’s thinking a lot about how to make web tools and web apps smarter, slicker and sexier.
+							<br>
+							<br> With a <b>BTech in CompSci</b>, He is already adept with various technology Stacks in Backend as well as FrontEnd, and is a sure shot to master many more in time! He plied his trade of web development at <b><a href="//solomosalsa.com" target="_blank" onClick="_gaq.push(['_trackEvent', 'Link', 'Click', 'solomosalsa']);">SoLoMoSalsa</a></b> before joining <b><a onClick="_gaq.push(['_trackEvent', 'Link', 'Click', 'lbb']);" target="_blank" href="https://lbb.in">Little Black Book</a></b>
+							<br>
+							<br>When Akarsh is not dreaming in algorithms and scripting, He is found contributing at different Tech events.
+							<br>He is working as a <b>Engineering Manager at Little Black Book</b> and answers to all the complicated engineering problems thrown at him there. He is also adept at DevOps and architecturing + building WebApps.
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="contact background color dark image quarter-opacity " id="contact" style="background-image:url(./assets/img/9.jpg)">
+		<div class="wrapper">
+			<div class="clearfix content">
+				<div class="">
+					<div class="clearfix description">
+						<h2>Contact</h2>
+						<p>Get in touch with me if you would like to do business together, share an idea or just say hello.</p>
+					</div>
+					<div class="info">
+						<?php /* <div class="location">
+								<h3>New Delhi Area, India</h3>
+								</div>
+								<div class="phone">
+								<h3><a href="tel:9017050076">9017050076</a></h3>
+								</div>
+								<div class="email">
+								<h3><a href="mailto:akarsh.satija@gmail.com">akarsh.satija@gmail.com</a></h3>
+								</div>
+						*/ ?>
+					</div>
+					<div class="social">
+						<a class="facebook" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'facebook']);" href="http://facebook.com/satija.akarsh" target="_blank"></a>
+						<a class="twitter" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'twitter']);" href="http://twitter.com/@akarshsatija" target="_blank"></a>
+						<a class="linkedin" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'linkedin']);" href="https://www.linkedin.com/in/akarshsatija" target="_blank"></a>
+						<a class="github" onClick="_gaq.push(['_trackEvent', 'Social', 'Go', 'github']);" href="http://github.com/akarshsatija" target="_blank"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<script src="./assets/jquery.min.js"></script>
+	<script>
+	(function(i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function() {
+			(i[r].q = i[r].q || []).push(arguments)
+		}, i[r].l = 1 * new Date();
+		a = s.createElement(o),
+			m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+	})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-    (function() {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
-    //smooth scroll
-    $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });
-    </script>
-    <link rel="stylesheet" media="screen" href="./bower_components/particles.js/demo/css/style.css">
-    <script src="./bower_components/particles.js/particles.js"></script>
-    <script src="./bower_components/particles.js/demo/js/app.js"></script>
+	ga('create', 'UA-46142311-1', 'auto');
+	ga('send', 'pageview');
+	</script>
+	<script type="text/javascript">
+	//smooth scroll
+	$(function() {
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+	</script>
+	<link rel="stylesheet" media="screen" href="./bower_components/particles.js/demo/css/style.css">
+	<script src="./bower_components/particles.js/particles.js"></script>
+	<script src="./bower_components/particles.js/demo/js/app.js"></script>
 </body>
 
 </html>
